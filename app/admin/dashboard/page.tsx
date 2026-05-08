982f15ff13ae3bc5d29fe936867d44a583226aeb
Eyad Nader <Eyad.nader2008@gmail.com>
2026-05-08 20:04:46 +0300

    Connect all pages

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function AdminDashboard() {
   return (
     <main className="min-h-screen bg-gray-100 p-10">
@@ -8,25 +10,29 @@ export default function AdminDashboard() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
 
-        <div className="bg-white p-8 rounded-3xl shadow">
-          <h2 className="text-3xl font-bold">
-            Products
-          </h2>
+        <Link href="/admin/products">
+          <div className="bg-white p-8 rounded-3xl shadow cursor-pointer hover:scale-105 transition">
+            <h2 className="text-3xl font-bold">
+              Products
+            </h2>
 
-          <p className="mt-3">
-            Add and manage products
-          </p>
-        </div>
+            <p className="mt-3">
+              Add and manage products
+            </p>
+          </div>
+        </Link>
 
-        <div className="bg-white p-8 rounded-3xl shadow">
-          <h2 className="text-3xl font-bold">
-            Orders
-          </h2>
+        <Link href="/admin/orders">
+          <div className="bg-white p-8 rounded-3xl shadow cursor-pointer hover:scale-105 transition">
+            <h2 className="text-3xl font-bold">
+              Orders
+            </h2>
 
-          <p className="mt-3">
-            View customer orders
-          </p>
-        </div>
+            <p className="mt-3">
+              View customer orders
+            </p>
+          </div>
+        </Link>
 
       </div>
 
